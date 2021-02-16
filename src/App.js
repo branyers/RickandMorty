9cebd655d5a89c3f924bd5e954e94051b107d9ba
Branyes N.A <branyernunez@gmail.com>
2021-02-16 00:01:40 -0400

    CambiosV4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import ResidentInfo from './components/ResidentInfo';
 import SearchBox from './components/SearchBox';
 import LocationContainer from './components/LocationContainer';
-import React,{useState, useEffect} from 'react'
+import React,{useState} from 'react'
 
 function App() {
 
   const [ value, setValue] = useState();
 
-  
+
 
 
   const Search = (e) => {
