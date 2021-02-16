65b74c069936788ce39a39466b96d65abd0779ec
Branyes N.A <branyernunez@gmail.com>
2021-02-16 00:16:32 -0400

    Cambiosv5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import SearchBox from './components/SearchBox';
 import LocationContainer from './components/LocationContainer';
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
 function App() {
 
   const [ value, setValue] = useState();
 
+  const planet = [
+  "Purge Planet,"
+  ,"Venzenulon 7"
+  ,"Bepis 9",
+  "Cronenberg Earth"
+  ,"Nuptia 4",
+  "Giant's Town",
+  "Bird World",
+  "St. Gloopy Noops Hospital",
+  "Earth (5-126",
+  "Mr. Goldenfold's dream",
+  "Gromflom Prime",
+  "Earth (Replacement Dimension)",
+  "Anatomy Park"
+]
+
+  useEffect(() => {
+    const stringThis = Math.floor(Math.random() * planet.length)
+  
+    setTimeout(() => {
+      setValue(planet[stringThis])
+    }, 500)
+  },[])
 
 
 
