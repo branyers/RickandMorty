1550667644119451efdb62fe6abe19bfb2d9ab04
Branyes N.A <branyernunez@gmail.com>
2021-02-16 00:03:34 -0400

    Cambiosv4

--- src/components/LocationContainer.jsx
+++ src/components/LocationContainer.jsx
@@ -33,18 +33,7 @@ const LocationContainer = ({ value }) => {
 
     }
 
-    const findLocation = (resultsData, locationSeach) => {
-        for (let i = 0; i < resultsData.length; i++) {
-          const resultSearch = resultsData[i].find(
-            (element) => locationSeach.toLowerCase() == element.name.toLowerCase()
-          );
-          if(resultSearch && resultSearch != -1) {
-            console.log(resultSearch);
-            return resultSearch
-          }
-        }
-        return false;
-      }
+
     
     useEffect(async () => {
 
@@ -52,7 +41,6 @@ const LocationContainer = ({ value }) => {
             getAPI().then((data) => {
             for(let i = 0; i<data.results.length; i++){
                 if(data.results[i].name == value){
-                    console.log(data.results[i])
                     poblateState(data.results[i])
                 }
 
